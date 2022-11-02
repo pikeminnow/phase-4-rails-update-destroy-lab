66c3eceec525e03ed336a9b19a140612f934f9b6
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-02 10:16:19 -0400

    lab activities

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,9 @@
+# app/controllers/plants_controller.rb #PlantsController controls routes for plant API
+
+# frozen_string_literal:false
+
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +13,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +23,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+
+  def update
+    plant = find_plant
+    plant.update(is_in_stock: plant_params[:is_in_stock])
+    render json: plant, status: :ok
+  end
+
+  # DELETE /plants/:id
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: 'Plant not found' }, status: :not_found
   end
 end
